0ef6e668f191da546e7aeeaa54fadfd11b8f442d
cranzoid <vishesh.vaibhav@iu-study.org>
2023-08-05 18:33:05 +0530

    static

--- certificate_generator/settings.py
+++ certificate_generator/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'certificate_app/static'
 STATIC_ROOT = [os.path.join(BASE_DIR, 'certificate_app/static')]
 
 # Default primary key field type
